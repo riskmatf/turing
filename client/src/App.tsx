c8909d03e9954308c9e66c46782ea665b9f5a9fe
Pedja Trifunov <trifunov.pedja@gmail.com>
2019-04-13 22:11:53 +0200

    Init commit for client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Here register all services that you need for program
+ * ServiceLocator.registerAudioService(new AudioServiceImpl())
+ */
+
+type Props = {};
+
+function App(props: Props): React.ReactElement
+{
+    return <h1>hello world</h1>
+}
+
+export {App};
