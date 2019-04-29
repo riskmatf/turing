6714e0cc7596be971b918d77376e5115fe69d077
Pedja Trifunov <trifunov.pedja@gmail.com>
2019-04-29 16:59:21 +0200

    Fixed style a little bit

--- client/src/components/home_page.tsx
+++ client/src/components/home_page.tsx
@@ -12,31 +12,39 @@ export function HomePage(props: Props): React.ReactElement
 {
     return (
         <React.Fragment>
+            <Grid container direction={'column'} justify={'center'}>
             {/*TODO - set new logo*/}
-            <div className='hv-center-container'>
-                <img src='/turing/assets/img/logo.png' alt='Turing Logo' />
-            </div>
+                <Grid item>
+                    <div className='hv-center-container'>
+                        <img src='/turing/assets/img/logo.png' alt='Turing Logo' />
+                    </div>
+                </Grid>
 
-            {/*TODO - fix the layout etc...*/}
-            <Paper style={{margin: '20px', width: '100%'}} elevation={1}>
-                <Typography variant="h4" component="h3">
-                    Turing
-                </Typography>
-                <Typography component="p">
-                   Projekat Turing je platforma namenjena prikazivanju informacija o računarskoj
-                   opremi u okviru Matematičkog fakulteta. Platforma u realnom vremenu omogućava
-                   administratorima i korisnicima da prate i ažuriraju stanje računarske opreme.
-                </Typography>
-                <Typography component="p">
-                </Typography>
-                <Typography component="p">
-                    Projekat je izveden u okviru
-                    organizacije <a target='_blank'href='http://risk.matf.bg.ac.rs/'>RISK</a>
-                    { ' ' } sa ciljem da unapredi funkcionisanje Računarske laboratorije
-                    i olakša održavanje računarske opreme.
-                </Typography>
-            </Paper>
+                <Grid container item justify={'center'}>
+                    <Grid item xs={6}>
+                        {/*TODO - fix the layout etc...*/}
+                        <Paper style={{width: '100%', padding: '10px'}} elevation={1}>
+                            <Typography variant="h4" component="h3">
+                                Turing
+                            </Typography>
+                            <Typography component="p">
+                               Projekat Turing je platforma namenjena prikazivanju informacija o računarskoj
+                               opremi u okviru Matematičkog fakulteta. Platforma u realnom vremenu omogućava
+                               administratorima i korisnicima da prate i ažuriraju stanje računarske opreme.
+                            </Typography>
+                            <Typography component="p">
+                            </Typography>
+                            <Typography component="p">
+                                Projekat je izveden u okviru
+                                organizacije <a target='_blank'href='http://risk.matf.bg.ac.rs/'>RISK</a>
+                                { ' ' } sa ciljem da unapredi funkcionisanje Računarske laboratorije
+                                i olakša održavanje računarske opreme.
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
 
+            </Grid>
         </React.Fragment>
     );
 }
