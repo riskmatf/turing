21a50ae94f103f76164db6fe094649203d6b89c7
Pedja Trifunov <trifunov.pedja@gmail.com>
2019-05-06 14:39:27 +0200

    Added models for admin side

---
+++ client/src/models/admin_side/classroom.ts
@@ -0,0 +1,8 @@
+export class Classroom
+{
+    public constructor(public name: string, public location: string,
+                       public schemaUrl: string, public computerCount: number)
+    {
+    }
+}
+
