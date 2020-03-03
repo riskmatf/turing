7497398f5c0dc628476fa2e0ec67f0e23b551461
Pedja Trifunov <pedjat@nordeus.com>
2020-03-03 18:49:07 +0100

    Added support for cors on all routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,12 @@ import { router } from './router';
 
 const app = express();
 const port = 3000;
+
+/*Adding cors support for all routes*/
+app.use('', (req, resp, next) => {
+  resp.set('Access-Control-Allow-Origin', '*')
+  next()
+})
 app.use("/api/v1", router);
 app.listen(port, err => {
   if (err) {
