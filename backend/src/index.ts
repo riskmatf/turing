7732824b2e9f92ddfcea21b09fbe0927d0b22b93
default <vnovakovic96@gmail.com>
2020-03-14 15:17:22 +0100

    [#20] implemented route for classrooms

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,38 @@
 import express from 'express';
 import { router } from './router';
 import { createConnection } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+export let imagesPaths : Map<string, string> = new Map<string, string>();
 
 createConnection().then( conn => {
 	const app = express();
 	const port = 3000;
 
+	
+	fs.readdir(path.join(__dirname, "public/images"),
+				(err, files)=>{
+					if(err){
+						return console.log("Cant open images!");
+					}
+					files.forEach(file => {
+						const indexOfDot = file.lastIndexOf(".");
+						if(indexOfDot){
+							const name = file.substr(0, indexOfDot);
+							imagesPaths.set(name, file);
+						}
+					});
+				});
+	
+
 	/*Adding cors support for all routes*/
 	app.use('', (req, resp, next) => {
 		resp.set('Access-Control-Allow-Origin', '*')
-		next()
+		console.log(req.url);
+		next();
 	})
+	app.use("/static", express.static(path.join(__dirname, "public")));
 	app.use("/api/v1", router);
 	app.listen(port, err => {
 		if (err) {
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,20 +1,40 @@
 import express from 'express';
 import { getRepository } from 'typeorm';
 import { Classroom } from '../entities/Classroom';
+import { imagesPaths } from '../src/index';
 const router = express.Router();
 
-router.use((req, _res, next)=>{
-	console.log("challenge : " + req.baseUrl);
-	next();
-});
+// router.use((req, _res, next)=>{
+// 	console.log("challenge : " + req.baseUrl);
+// 	next();
+// });
 
 router.get("/", (_req, res)=>{
 	res.send("Hello from the otter side");
 });
 
-router.get("/classrooms", (_req, res)=>{
+router.get("/classrooms", (req, res)=>{
 	let classroomsRepo = getRepository(Classroom);
-	classroomsRepo.find().then(dbRes => res.send(dbRes));
+	classroomsRepo.find({relations: ["computers"]}).then(classrooms=>{
+		const baseUrl = req.protocol + "://" + req.get("host") + "/";
+		let mappedClassrooms = classrooms.map(classroom=>{
+			let numOfBroken = classroom.computers.reduce((acc, comp) => acc += (comp.broken ? 1 : 0)
+														, 0);
+			let numOfWorking = classroom.computers.length - numOfBroken;
+			const schemaUrl = baseUrl + `static/schemas/${classroom.name}.svg`
+			return {
+				name: classroom.name,
+				location: classroom.location,
+				numberOfComputers: {
+					working: numOfWorking,
+					broken: numOfBroken
+				},
+				imageUrl: baseUrl + "static/images/" + imagesPaths.get(classroom.name),
+				schemaUrl: schemaUrl,
+			}
+		})
+		res.send(mappedClassrooms);
+	});
 });
 
 export {router};
