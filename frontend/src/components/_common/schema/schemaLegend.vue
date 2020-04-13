fb08eb201251ee56d1b9c4f2f1525967d3a805ff
basilisk6 <basilisk@DESKTOP-4518NN0.localdomain>
2020-04-13 09:17:52 -0700

    Added legend to classrom pages

--- frontend/src/components/_common/schema/schemaLegend.vue
+++ frontend/src/components/_common/schema/schemaLegend.vue
@@ -1,8 +1,31 @@
 <template>
-    <div>Legend</div>
+    <div class="legend">
+            <div>
+                <img src='@/assets/yellowSign.svg' alt="yellowSign"> 
+            </div>
+            <div class="text">    
+                Za popravku 
+            </div>
+            <div class="tabsp">
+                <img src='@/assets/redSign.svg' alt="redSign">    
+            </div>
+            <div class="text">
+                Izbacen iz upotrebe 
+            </div>
+    </div>
 </template>
 
 <style lang="sass" scoped>
+    .legend
+        display: flex
+        flex-direction: row
+        padding-left: 5px
+        justify-content: center
+    .text
+        padding-top: 5px
+        padding-left: 5px
+    .tabsp
+        padding-left: 25px
 
 </style>
 
