1285c5780158be8e0d5a88a14970463ad0561f38
Pedja Trifunov <pedjat@nordeus.com>
2020-04-18 22:46:11 +0200

    Fixed style of the top bar

--- frontend/src/components/_common/baseLayout/baseLayout.vue
+++ frontend/src/components/_common/baseLayout/baseLayout.vue
@@ -1,24 +1,53 @@
 <template>
     <div class="column full">
-        <div class="topBar desktop">
-            <router-link :to="{ name: 'homePage' }" tag="div">
-                <h1 class="home-logo">Turing</h1>
-            </router-link>
+        <el-menu
+            :default-active="$route.meta.index"
+            mode="horizontal"
+            route
+            class="desktop topBar"
+            active-text-color="#409EFF"
+        >
+            <el-menu-item index="0" :route="{ name: 'homePage' }">
+                Turing logo
+            </el-menu-item>
             <div class="spacer"></div>
             <slot name="topBar"/>
-        </div>
+        </el-menu>
 
         <div class="topBar mobile">
-            <div>Turing</div>
-            <div class="spacer"></div>
-            <mobile-drop-nav>
-                <template v-slot>
-                    <slot name="topBar"/>
-                </template>
-            </mobile-drop-nav>
+            <el-menu
+                :default-active="$route.meta.index"
+                mode="horizontal"
+                router
+                class="mobile topBar"
+                active-text-color="#409EFF"
+                @select="handleSelect"
+            >
+                <el-menu-item index="0" :route="{ name: 'homePage' }">
+                    Turing logo
+                </el-menu-item>
+                <div class="spacer"></div>
+                <el-button size="mini" @click="isOpen = !isOpen">=</el-button>
+            </el-menu>
         </div>
 
         <div class="column full body-padding">
+            <el-popover
+                v-model="isOpen"
+                trigger="manual"
+                popper-class="topbar-dropdown mobile"
+                :append-to-body="false"
+                :visible-arrow="false"
+            >
+                <el-menu
+                    :default-active="$route.meta.index"
+                    router
+                    active-text-color="#409EFF"
+                    @select="handleSelect"
+                >
+                    <slot name="topBar"/>
+                </el-menu>
+            </el-popover>
             <slot name="body"></slot>
         </div>
     </div>
@@ -40,22 +69,21 @@
         display: flex
         flex-direction: row
     .topBar
+        width: 100%
         display: flex
         flex-direction: row
-        justify-content: flex-end
+        justify-content: space-between
         align-items: center
-        background-color: $el-background-color
+        .spacer
+            flex-grow: 1
     .mobile
         display: none
-        height: $topBarHeight
         @media ($mobileBreakPoint)
             display: flex
     .desktop
         display: flex
         @media ($mobileBreakPoint)
             display: none
-    .spacer
-        flex-grow: 2
     .home-logo
         margin: 2px
         cursor: pointer
@@ -72,16 +100,32 @@
         padding: 0
         margin: 0
         background-color: $background-color
+
+    .topbar-dropdown
+        width: 100%
+        height: 100%
+        padding: 0!important
+        margin: 0!important
+        margin-left: -5px!important
+        box-sizing: border-box
+
 </style>
 
 
 <script>
-    import MobileDropNav from './mobileDropNav'
-
     export default {
         name: 'base-layout',
         components: {
-            MobileDropNav
+        },
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+        methods: {
+            handleSelect() {
+                this.isOpen = false
+            }
         }
     }
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,23 +24,23 @@ const routes = [
         path: '/',
         component: BaseLayout,
         children: [
-            { path: '', name: 'homePage', component: HomePage },
+            { path: '', name: 'homePage', component: HomePage, meta: { index: '1' }},
             {
                 path: '/classrooms',
                 component: RouterView,
                 children: [
-                    { path: '', name: 'classroomListPage', component: ClassroomListPage },
+                    { path: '', name: 'classroomListPage', component: ClassroomListPage, meta: { index: '2' } },
                     {
                         path: ':classroomId',
                         component: RouterView,
                         children: [
-                            { path: '', name: 'classroomPage', component: ClassroomPage},
-                            { path: ':computerId', name: 'computerPage', component: ReportListPage }
+                            { path: '', name: 'classroomPage', component: ClassroomPage, meta: { index: '2' }},
+                            { path: ':computerId', name: 'computerPage', component: ReportListPage, meta: { index: '2' } }
                         ]
                     },
                 ]
             },
-            { path: '/tutorial', name: 'tutorialPage', component: TutorialPage},
+            { path: '/tutorial', name: 'tutorialPage', component: TutorialPage, meta: { index: '3' }},
         ]
     }
 ]
