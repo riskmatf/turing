0731e024a051e2e8a92df009fffcf94b934c1795
Pedja Trifunov <pedjat@nordeus.com>
2020-05-15 22:01:53 +0200

    Merge fix

--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -1,9 +1,11 @@
 import BaseLayout from './baseLayout'
 import HomePage from './homePage'
 import { ClassroomListPage } from './classroomListPage'
+import LoginPage from './loginPage'
 
 export {
     BaseLayout,
     HomePage,
     ClassroomListPage,
+    LoginPage,
 }
