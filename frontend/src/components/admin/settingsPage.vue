ed97550ecd027c031a300167039e9a6a3199e199
Luka Sokolov <lukasokolov@gmail.com>
2020-05-17 16:41:48 +0200

    Finished style of adminSettingsPage

--- frontend/src/components/admin/settingsPage.vue
+++ frontend/src/components/admin/settingsPage.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        <!-- Change display name, and change password -->
         <page-header>
             <breadcrumbs :paths="breadcrumbData" class="breadcrumbs"/>
         </page-header>
         <div v-if="request.status === 'success'" class="location-container">
-            <el-card class="content-container">Promeni</el-card>
+            <el-card class="content-container">
+                <el-collapse>
+                    <el-collapse-item title="Promena imena">
+                        <div class="collapse-container">
+                            <div class="marginer">Unesite novo ime:</div>
+                            <el-input placeholder="Novo ime" size="mini" class="input"></el-input> 
+                            <el-button size="mini" class="styler">Promeni ime</el-button>
+                        </div>
+                    </el-collapse-item>
+                    <el-collapse-item title="Promena lozinke">
+                        <div class="collapse-container">
+                            <div class="marginer">Unesite novu lozinku:</div>
+                            <el-input placeholder="Nova lozinka" size="mini" class="input"></el-input> 
+                            <div class="marginer">Potvrdite novu lozinku:</div>
+                            <el-input placeholder="Potvrda lozinke" size="mini" class="input"></el-input> 
+                            <el-button size="mini" class="styler">Promeni lozinku</el-button>                  
+                        </div>
+                    </el-collapse-item>
+                </el-collapse>
+            </el-card>
         </div>
         <template v-else-if="request.status === 'loading'">
             <div class="loading">
@@ -30,6 +48,17 @@
         margin-top: 5px
         .content-container
             width: 90%
+            .collapse-container
+                display: flex
+                flex-direction: column
+                width: 90%
+                margin: auto
+            .styler
+                margin-top: 10px
+                align-self: flex-end
+            .marginer
+                margin-top: 2px
+                margin-bottom: 2px
     .loading
         margin: 20px
 </style>
